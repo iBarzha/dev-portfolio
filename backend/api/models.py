a13e3b9e5d374551e4539ae27807027b486f835a
iBarzha <grytcaie@gmail.com>
2025-07-15 21:58:09 +0300

    setup backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    github_link = models.URLField()
+    demo_link = models.URLField(blank=True)
+    image = models.ImageField(upload_to='projects/')
+
+    def __str__(self):
+        return self.title
