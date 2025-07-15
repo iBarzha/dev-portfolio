58772a29e71a34c21eab3cac94333ec39de00ca5
iBarzha <grytcaie@gmail.com>
2025-07-15 21:55:43 +0300

    setup frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00BFFF',
+        secondary: '#1E293B',
+        accent: '#38BDF8'
+      },
+    },
+  },
+  plugins: [],
+}
